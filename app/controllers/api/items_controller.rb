295df9e296ba30043cdba0bca15f9db086f6042a
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-10 16:04:21 -0600

    controller setup

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,7 +1,17 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_list, only: [:index]
   def index
+    render json: @list.items.all
   end
 
   def show
   end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+
 end
--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,5 +1,6 @@
 class Api::ListsController < ApplicationController
   def index
+    render json: List.all
   end
 
   def show

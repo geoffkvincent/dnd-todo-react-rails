d421370cf359c7ad88022c3cb627f34bb6ad1a91
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-11 09:20:08 -0600

    axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,117 +1,34 @@
 import React from 'react';
 import styled from 'styled-components'
-import initialData from './initial-data'
-import Column from './column'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+
+import axios from 'axios'
 
 const Container = styled.div`
   display: flex;
 `;
 
 class App extends React.Component {
-  state = initialData;
+  state = { lists: [] };
 
-  onDragEnd = result => {
-    const { destination, source, draggableId, type } = result;
-
-    if (!destination) {
-      return;
-    }
-
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-
-    if (type === 'column') {
-      const newColumnOrder = Array.from(this.state.columnOrder)
-      newColumnOrder.splice(source.index, 1)
-      newColumnOrder.splice(destination.index, 0, draggableId)
-
-      const newState = {
-        ...this.state,
-        columnOrder: newColumnOrder,
-      }
-      this.setState(newState)
-      return
-    }
-
-    const start = this.state.columns[source.droppableId]
-    const finish = this.state.columns[destination.droppableId]
-
-    if (start === finish) {
-      const newTaskIds = Array.from(start.taskIds)
-      newTaskIds.splice(source.index, 1)
-      newTaskIds.splice(destination.index, 0, draggableId)
-
-      const newColumn = {
-        ...start,
-        taskIds: newTaskIds,
-      }
-
-      const newState = {
-        ...this.state,
-        columns: {
-          ...this.state.columns,
-          [newColumn.id]: newColumn,
-        }
-      };
-
-      this.setState(newState)
-      return
-    }
-
-    const startTaskIds = Array.from(start.taskIds)
-    startTaskIds.splice(source.index, 1)
-    const newStart = {
-      ...start,
-      taskIds: startTaskIds,
-    }
-
-    const finishTaskIds = Array.from(finish.taskIds)
-    finishTaskIds.splice(destination.index, 0, draggableId)
-    const newFinish = {
-      ...finish,
-      taskIds: finishTaskIds,
-    }
-
-    const newState = {
-      ...this.state,
-      columns: {
-        ...this.state.columns,
-        [newStart.id]: newStart,
-        [newFinish.id]: newFinish,
-      },
-    }
-    this.setState(newState)
+  componentDidMount() {
+    axios.get('/api/lists')
+      .then( ({data: lists}) => this.setState({lists}) )
   }
+
+  
 
   render() { 
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable 
-          droppableId="all-columns" 
-          direction="horizontal" 
-          type="column"
-        >
-          {provided => (
-            <Container
-              {...provided.droppableProps}
-              innerRef={provided.innerRef}
-            >
-              {this.state.columnOrder.map((columnId, index) => {
-                const column = this.state.columns[columnId];
-                const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      <Container>
+        {this.state.columnOrder.map((columnId, index) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-                return <Column key={column.id} column={column} tasks={tasks} index={index} />
-              })}
-              {provided.placeholder}
-            </Container>
-          )}
-        </Droppable>
-      </DragDropContext>
+          
+          
+        })}
+      </Container>
+    
     )
   }   
 }
